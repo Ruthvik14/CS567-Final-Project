624612b96d7d1abe07cd9b8d4dcb9c0413154a63
Ruthvik14 <44079999+Ruthvik14@users.noreply.github.com>
2024-05-06 21:59:47 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,16 @@
+-- Create the Categories table
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
 DROP TABLE IF EXISTS items;
 CREATE TABLE items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT UNIQUE,
     price REAL,
-    quantity INTEGER
+    quantity INTEGER,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
+
